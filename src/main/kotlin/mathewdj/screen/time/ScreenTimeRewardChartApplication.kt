3fc7389ed52f7eeed99612967a2ba888b5bf1b85
mathewdj <9652156+green-seagull@users.noreply.github.com>
2022-01-16 08:46:04 +1100

    Spring boot initializr init project

---
+++ src/main/kotlin/mathewdj/screen/time/ScreenTimeRewardChartApplication.kt
@@ -0,0 +1,11 @@
+package mathewdj.screen.time
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScreenTimeRewardChartApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScreenTimeRewardChartApplication>(*args)
+}
---
+++ src/test/kotlin/mathewdj/screen/time/ScreenTimeRewardChartApplicationTests.kt
@@ -0,0 +1,13 @@
+package mathewdj.screen.time
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScreenTimeRewardChartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
